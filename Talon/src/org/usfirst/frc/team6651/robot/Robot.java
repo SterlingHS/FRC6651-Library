c85339184eb03f2a2119f102877f05c3aabfa82b
Alexis Rivera <arivera@sps5.org>
2018-11-10 17:48:21 -0600

    Clean Up Talon
    Cleaned up in Talon

--- Talon/src/org/usfirst/frc/team6651/robot/Robot.java
+++ Talon/src/org/usfirst/frc/team6651/robot/Robot.java
@@ -18,20 +18,12 @@ import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
 
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the IterativeRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the build.properties file in the
- * project.
- */
+
 public class Robot extends IterativeRobot {
 	
 	public static DifferentialDrive DT;
-	// Spark slider;
+
 	Joystick PS4 = new Joystick(0);
-	// int buttonButterflyId = 1;
-	// Button butterflyButton = new JoystickButton(PS4, buttonButterflyId);
 	Button butterflyButtonUp = new JoystickButton(PS4, 1);
 	Button butterflyButtonDown = new JoystickButton(PS4, 2);
 	
@@ -45,9 +37,6 @@ public class Robot extends IterativeRobot {
 	boolean changeOfState = true;
 	boolean butterflyState = false;
 
-	// c.setClosedLoopControl(true);  // Start compressor control
-	// c.setClosedLoopControl(false); // Stop compressor control
-	// exampleSolenoid.set(true);  or  exampleSolenoid.set(false);
 	
 	@Override
 	public void robotInit() {
@@ -67,52 +56,25 @@ public class Robot extends IterativeRobot {
 		butterflySolenoid.set(DoubleSolenoid.Value.kForward);	
 	}
 
-	/**
-	 * This autonomous (along with the chooser code above) shows how to select
-	 * between different autonomous modes using the dashboard. The sendable
-	 * chooser code works with the Java SmartDashboard. If you prefer the
-	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-	 * getString line to get the auto name from the text box below the Gyro
-	 *
-	 * <p>You can add additional auto modes by adding additional comparisons to
-	 * the switch structure below with additional strings. If using the
-	 * SendableChooser make sure to add them to the chooser code above as well.
-	 */
+
 	@Override
 	public void autonomousInit() {
 		
 	}
 
-	/**
-	 * This function is called periodically during autonomous.
-	 */
+
 	@Override
 	public void autonomousPeriodic() {
 
 	}
 
-	/**
-	 * This function is called periodically during operator control.
-	 */
+
 	@Override
 	public void teleopPeriodic() {
 		int X_axis = 1, Y_axis = 0, Rotation = 2, Throttle = 3;
 		double forward = PS4.getRawAxis(X_axis); 
 		double turn = PS4.getRawAxis(Y_axis); 
 		DT.arcadeDrive(forward, -turn);
-/*
-		if (PS4.getRawButton(buttonButterflyId) == true && changeOfState == true) {
-			System.out.println("Button pushed");
-			if (butterflyState == false)
-				butterflySolenoid.set(DoubleSolenoid.Value.kForward);
-			else
-				butterflySolenoid.set(DoubleSolenoid.Value.kReverse);
-			butterflyState = !butterflyState;
-			changeOfState = false;
-		}
-		if (PS4.getRawButton(buttonButterflyId) == false && changeOfState == false) {
-			changeOfState = true;
-		}*/
 		
 		if (PS4.getRawButton(1) == true) {
 			butterflySolenoid.set(DoubleSolenoid.Value.kForward);
@@ -122,9 +84,7 @@ public class Robot extends IterativeRobot {
 		}
 	}
 
-	/**
-	 * This function is called periodically during test mode.
-	 */
+
 	@Override
 	public void testPeriodic() {
 	}
