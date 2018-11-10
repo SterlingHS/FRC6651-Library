d778822c76f61624ef91cff7c4feee77eb9f6d1c
Alexis Rivera <arivera@sps5.org>
2018-11-10 13:18:31 -0600

    Clean Up
    Cleaned up file

--- DrivePS4/src/org/usfirst/frc/team6651/robot/Robot.java
+++ DrivePS4/src/org/usfirst/frc/team6651/robot/Robot.java
@@ -14,13 +14,6 @@ import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
 
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the IterativeRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the build.properties file in the
- * project.
- */
 public class Robot extends IterativeRobot {
 	
 	public static DifferentialDrive DT;
@@ -41,36 +34,21 @@ public class Robot extends IterativeRobot {
 		slider = new Spark(6);
 	}
 
-	/**
-	 * This autonomous (along with the chooser code above) shows how to select
-	 * between different autonomous modes using the dashboard. The sendable
-	 * chooser code works with the Java SmartDashboard. If you prefer the
-	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-	 * getString line to get the auto name from the text box below the Gyro
-	 *
-	 * <p>You can add additional auto modes by adding additional comparisons to
-	 * the switch structure below with additional strings. If using the
-	 * SendableChooser make sure to add them to the chooser code above as well.
-	 */
 	@Override
 	public void autonomousInit() {
 		
 	}
 
-	/**
-	 * This function is called periodically during autonomous.
-	 */
+
 	@Override
 	public void autonomousPeriodic() {
 
 	}
 
-	/**
-	 * This function is called periodically during operator control.
-	 */
+
 	@Override
 	public void teleopPeriodic() {
-		int X_axis = 1, Y_axis = 0, Rotation = 2, Throttle = 3;
+		int X_axis = 1, Y_axis = 0, Rotation = 2;
 		double left = PS4.getRawAxis(X_axis); 
 		double right = PS4.getRawAxis(Rotation); 
 		DT.tankDrive(left,right);
@@ -79,9 +57,7 @@ public class Robot extends IterativeRobot {
 		slider.set(slideSpeed);
 	}
 
-	/**
-	 * This function is called periodically during test mode.
-	 */
+	
 	@Override
 	public void testPeriodic() {
 	}
