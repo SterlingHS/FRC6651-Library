65fa0029f015e19c98e85e3b00674d7dd9d6c62e
Alexis Rivera <arivera@sps5.org>
2018-11-10 13:39:35 -0600

    Clean Drivetrain
    Cleaned files in Drivetrain

--- Drivetrain/src/org/usfirst/frc/team6651/robot/Robot.java
+++ Drivetrain/src/org/usfirst/frc/team6651/robot/Robot.java
@@ -11,13 +11,6 @@ import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the IterativeRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the build.properties file in the
- * project.
- */
 public class Robot extends IterativeRobot {
 	private static final String kDefaultAuto = "Default";
 	private static final String kCustomAuto = "My Auto";
@@ -30,11 +23,6 @@ public class Robot extends IterativeRobot {
 	Joystick joy1 = new Joystick(1);
 	Joystick joy2 = new Joystick(0);
 
-	
-	/**
-	 * This function is run when the robot is first started up and should be
-	 * used for any initialization code.
-	 */
 	@Override
 	public void robotInit() {
 		Spark rightFront = new Spark(1);
@@ -49,22 +37,10 @@ public class Robot extends IterativeRobot {
 		slider = new Spark(6);
 	}
 
-	/**
-	 * This autonomous (along with the chooser code above) shows how to select
-	 * between different autonomous modes using the dashboard. The sendable
-	 * chooser code works with the Java SmartDashboard. If you prefer the
-	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-	 * getString line to get the auto name from the text box below the Gyro
-	 *
-	 * <p>You can add additional auto modes by adding additional comparisons to
-	 * the switch structure below with additional strings. If using the
-	 * SendableChooser make sure to add them to the chooser code above as well.
-	 */
+
 	@Override
 	public void autonomousInit() {
 		m_autoSelected = m_chooser.getSelected();
-		// autoSelected = SmartDashboard.getString("Auto Selector",
-		// defaultAuto);
 		System.out.println("Auto selected: " + m_autoSelected);
 	}
 
@@ -75,18 +51,13 @@ public class Robot extends IterativeRobot {
 	public void autonomousPeriodic() {
 		switch (m_autoSelected) {
 			case kCustomAuto:
-				// Put custom auto code here
 				break;
 			case kDefaultAuto:
 			default:
-				// Put default auto code here
 				break;
 		}
 	}
 
-	/**
-	 * This function is called periodically during operator control.
-	 */
 	
 	@Override
 	public void teleopPeriodic() {
@@ -136,9 +107,7 @@ public class Robot extends IterativeRobot {
 		slider.set(slideSpeed);
 	}
 
-	/**
-	 * This function is called periodically during test mode.
-	 */
+
 	@Override
 	public void testPeriodic() {
 		
